82857d8876534a9f66ea0f272c5bf865a2858614
Rai <raimi.rarara@gmail.com>
2021-10-16 17:25:08 +0900

    feat: topページresponsive対応

--- src/components/AdArea.tsx
+++ src/components/AdArea.tsx
@@ -1,15 +1,19 @@
 import { Box } from "@chakra-ui/layout";
 
 type Props = {
-  width: string;
-  height: string;
+  width: string[];
+  height: string[];
   imagePath?: string;
   text: string;
 };
 
 export default function AdArea(props: Props) {
   return (
-    <Box w={props.width} h={props.height} bg="red.300">
+    <Box
+      w={[props.width[0], props.width[1]]}
+      h={[props.height[0], props.height[1]]}
+      bg="red.300"
+    >
       {props.text}
     </Box>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import {
   Divider,
   Flex,
   Heading,
+  Spacer,
   Text,
 } from "@chakra-ui/layout";
 import type { NextPage } from "next";
@@ -19,8 +20,11 @@ import ImageSlider from "../components/ImageSlider";
 import IntroLinkBox from "../components/IntroLinkBox";
 import Footer from "../components/Footer";
 import KadaiInfoBox from "../../public/images/kadaiinfobox.png";
+import { useBreakpointValue } from "@chakra-ui/media-query";
 
 const Home: NextPage = () => {
+  const hiddenAddAreaB = useBreakpointValue({ sm: false, xl: true });
+
   return (
     <Box w="100%" h="100%">
       <Flex position="fixed" right="2.5" zIndex="overlay">
@@ -32,97 +36,152 @@ const Home: NextPage = () => {
         />
       </Flex>
 
-      <Box marginTop="18px" />
+      <Box marginTop="18" />
       <Heading>
-        <Center fontSize="xl" color="lightblue">
+        <Center fontSize={["xl", "4xl"]} color="lightblue">
           KADAI INFO
         </Center>
       </Heading>
       <Box marginTop="18px" />
-      <Box w="100%" h="300px" bgColor="gray.200">
+      <Box w="100%" h={["300px", "500px"]} bgColor="gray.200">
         <Center>Top画像</Center>
       </Box>
-      <Box marginTop="36px" />
-      <Center>
+      <Box marginTop={["36px", "36px"]} />
+      <Flex flexDir={["column", "row"]}>
+        <Flex justify={["center", "flex-start"]} marginTop={["0", "56"]}>
+          <HeadingBox />
+        </Flex>
+        <Box w={["100%", "80%"]} h={["250px"]} marginTop={["0", "56"]}>
+          <Center>
+            <Box w={["90%", "70%"]}>
+              <Text fontWeight="bold" fontSize="sm">
+                New!!
+              </Text>
+              <NewsLine />
+              <NewsLine />
+              <NewsLine />
+              <Box marginTop="8px" />
+              <Text fontWeight="bold" fontSize="sm">
+                もっと見る
+              </Text>
+            </Box>
+          </Center>
+        </Box>
+        <Box w="100%">
+          <Center>
+            <AdArea
+              width={["300px", "600px"]}
+              height={["250px", "500px"]}
+              text="広告A"
+            />
+          </Center>
+        </Box>
+      </Flex>
+      <Box marginTop={["36px", "36px"]} />
+      <Flex justify={["center", "initial"]}>
         <HeadingBox />
-      </Center>
-      <Box w="100%" h="250px">
-        <Center>
-          <Box w="90%">
-            <Text fontWeight="bold" fontSize="sm">
-              New!!
-            </Text>
-            <NewsLine />
-            <NewsLine />
-            <NewsLine />
-            <Box marginTop="8px" />
-            <Text fontWeight="bold" fontSize="sm">
-              もっと見る
-            </Text>
-          </Box>
-        </Center>
-      </Box>
-      <Box w="100%">
-        <Center>
-          <AdArea width="300px" height="250px" text="広告A" />
-        </Center>
-      </Box>
-      <Box marginTop="36px" />
-      <Center>
-        <HeadingBox />
-      </Center>
+      </Flex>
       <Box marginTop="36px" />
       <ImageSlider heading="Girls" hedingColor="red.600" />
       <Box marginTop="30px" />
       <ImageSlider heading="Boys" hedingColor="blue.600" />
       <Box marginTop="36px" />
-      <Center>
+      <Flex justify={["center", "initial"]}>
         <HeadingBox />
-      </Center>
-      <Box w="100%" h="250px" bgColor="gray.300">
-        投票説明画像
-      </Box>
+      </Flex>
+      <Flex justify={["initial", "center"]}>
+        <Text w={["100%", "350px"]} h="250px" bgColor="gray.300">
+          投票説明画像
+        </Text>
+      </Flex>
+      <Flex marginTop="36px" hidden={!hiddenAddAreaB}>
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        <AdArea
+          width={["320px", "640px"]}
+          height={["100px", "200px"]}
+          text="広告B①"
+        />
+        <Spacer />
+        <AdArea
+          width={["320px", "640px"]}
+          height={["100px", "200px"]}
+          text="広告B②"
+        />
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        <Spacer />
+      </Flex>
       <Box marginTop="36px" />
-      <Center>
+      <Flex justify={["center", "initial"]}>
         <HeadingBox />
-      </Center>
-      <Box w="100%" h="250px" bgColor="gray.300">
-        スケジュール表
-      </Box>
+      </Flex>
+      <Flex justify={["initial", "center"]}>
+        <Text w={["100%", "350px"]} h="250px" bgColor="gray.300">
+          スケジュール表
+        </Text>
+      </Flex>
       <Box marginTop="12px" />
+
       <IntroLinkBox text="美男美女SNAPとは" />
+
       <Box marginTop="36px" />
-      <Box w="100%">
+      <Box w="100%" hidden={hiddenAddAreaB}>
         <Center>
-          <AdArea width="320px" height="100px" text="広告B①" />
+          <AdArea
+            width={["320px", "640px"]}
+            height={["100px", "200px"]}
+            text="広告B①"
+          />
         </Center>
       </Box>
       <Box marginTop="12px" />
-      <Box w="100%">
+      <Box w="100%" hidden={hiddenAddAreaB}>
         <Center>
-          <AdArea width="320px" height="100px" text="広告B②" />
+          <AdArea
+            width={["320px", "640px"]}
+            height={["100px", "200px"]}
+            text="広告B②"
+          />
         </Center>
       </Box>
       <Box marginTop="12px" />
-      <Box w="100%">
-        <Center>
-          <AdArea width="234px" height="60px" text="広告C①" />
-        </Center>
-      </Box>
-      <Box marginTop="12px" />
-      <Box w="100%">
-        <Center>
-          <AdArea width="234px" height="60px" text="広告C②" />
-        </Center>
-      </Box>
-      <Box marginTop="56px" />
-      <Box w="100%" h="250px" position="relative" bgColor="gray.300">
-        <Box position="absolute" top="-25px">
+      <Flex flexDir={["column", "row"]}>
+        <Box w="100%">
           <Center>
-            <HeadingBox />
+            <AdArea
+              width={["234px", "540px"]}
+              height={["60px", "180px"]}
+              text="広告C①"
+            />
           </Center>
         </Box>
-        KADAI INFOの紹介画像
+        <Box marginTop="12px" />
+        <Box w="100%">
+          <Center>
+            <AdArea
+              width={["234px", "540px"]}
+              height={["60px", "180px"]}
+              text="広告C②"
+            />
+          </Center>
+        </Box>
+      </Flex>
+      <Box marginTop="56px" />
+      <Box w="100%" position="relative">
+        <Box position="absolute" top="-25px">
+          <Flex justify={["center", "initial"]}>
+            <HeadingBox />
+          </Flex>
+        </Box>
+        <Flex justify={["initial", "center"]}>
+          <Text w={["100%", "350px"]} h="250px" bgColor="gray.300">
+            KADAI INFOの紹介画像
+          </Text>
+        </Flex>
       </Box>
       <Box marginTop="12px" />
       <IntroLinkBox text="KADAI INFOとは" />
