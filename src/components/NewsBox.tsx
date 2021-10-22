15babd6c2fe6963b4bd6b9fedf252fbaf63bd765
Rai <raimi.rarara@gmail.com>
2021-10-23 01:20:37 +0900

    feat: newsbox

--- src/components/NewsBox.tsx
+++ src/components/NewsBox.tsx
@@ -1,20 +1,29 @@
 import { Box, Center, Text } from "@chakra-ui/layout";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import NewsBoxOutline from "../../public/svgs/news_box.svg";
+import { useState } from "react";
+import NewsBoxOutline from "./NewsBoxOutline";
 import NewsLine from "./NewsLine";
 
 export default function NewsBox() {
-  const news = ["a", "a", "a", "a", "a", "a", "a", "a"];
+  const [news, setNews] = useState(["a", "a", "a", "a", "a", "a", "a", "a"]);
+  const [publicNews, setPublicNews] = useState(["a", "a", "a"]);
   const [open, setOpen] = useState(false);
 
-  const handleClick = () => {
-    setOpen(!open);
+  const viewBox: string = "0 0 345 " + (publicNews.length * 100).toString();
+  const height: string = (publicNews.length * 88).toString();
+
+  const handleOpen = () => {
+    setOpen(true);
+    setPublicNews(news);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setPublicNews(news.slice(0, 3));
   };
 
   return (
     <Box w={["90%", "70%"]} position="relative" margin="auto">
-      <Image src={NewsBoxOutline} />
+      <NewsBoxOutline viewBox={viewBox} height={height} />
       <Box
         w={["95%", "80%"]}
         h={["250px"]}
@@ -27,16 +36,16 @@ export default function NewsBox() {
             <Text fontWeight="bold" fontSize="sm">
               New!!
             </Text>
-            {open && news.length > 0
-              ? news.map((value, index) => <NewsLine />)
-              : news.slice(0, 3).map((value, index) => <NewsLine />)}
+            {open && publicNews.length > 0
+              ? publicNews.map((index) => <NewsLine height={14} />)
+              : publicNews.map((index) => <NewsLine height={12} />)}
             <Box marginTop="8px" />
             {open ? (
-              <Text fontWeight="bold" fontSize="sm" onClick={handleClick}>
+              <Text fontWeight="bold" fontSize="sm" onClick={handleClose}>
                 とじる
               </Text>
             ) : (
-              <Text fontWeight="bold" fontSize="sm" onClick={handleClick}>
+              <Text fontWeight="bold" fontSize="sm" onClick={handleOpen}>
                 もっと見る
               </Text>
             )}
