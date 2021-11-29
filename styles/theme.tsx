9ba7ef3a7ab78215bd95dbaf42b8aaafceb9a471
Rai <raimi.rarara@gmail.com>
2021-11-30 03:14:44 +0900

    fix: font

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -5,7 +5,7 @@ import { extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
   fonts: {
     heading: "irohakakuC-Bold",
-    body: "'YuGothic','Yu Gothic Medium', 'Yu Gothic','游ゴシック体', '游ゴシック Medium', '游ゴシック', 'Noto Sans JP','Roboto',sans-serif",
+    body: "'YuGothic','Yu Gothic Medium', 'Yu Gothic','游ゴシック体', '游ゴシック Medium', '游ゴシック','Noto Serif JP', serif",
   },
   colors: {
     base: "#fff",
