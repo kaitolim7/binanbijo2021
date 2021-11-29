3cae4cc79ec55e6e560808de6751fe5089541cbd
Rai <raimi.rarara@gmail.com>
2021-11-30 00:03:33 +0900

    feat: googlefont

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,18 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import Fonts from "../../styles/fonts";
 import theme from "../../styles/theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
