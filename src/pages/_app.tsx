26be35feb64f76c29d7e262cdfa292cb61daf1fb
Rai <raimi.rarara@gmail.com>
2021-11-30 11:30:08 +0900

    fix:font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -30,9 +30,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@400;500;600&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500&display=swap"
           rel="stylesheet"
-        />
+        ></link>
       </Head>
       <Component {...pageProps} />
     </ChakraProvider>
