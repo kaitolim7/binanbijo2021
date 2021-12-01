d8ed202f0a04a5081d5bab17d016238613d04ff4
Rai <raimi.rarara@gmail.com>
2021-12-01 17:44:28 +0900

    fix: twitter image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,10 @@ export default function Header(props: Props) {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={"../" + image} />
+        <meta
+          name="twitter:image"
+          content={"../../public/images/og_image.jpg"}
+        />
       </Head>
       {!props.disable && (
         <Box>
--- src/pages/2021/index.tsx
+++ src/pages/2021/index.tsx
@@ -1,10 +1,11 @@
 import { useRouter } from "next/dist/client/router";
 import { useEffect } from "react";
+import Header from "../../components/Header";
 
 export default function Redirect() {
   const router = useRouter();
   useEffect(() => {
     router.push("/");
   }, []);
-  return <></>;
+  return <Header title={""} disable={true} />;
 }
